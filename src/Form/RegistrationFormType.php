918b8acf2fe08d025ce4f1f7bc61e541d3ca38f2
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-11-05 13:56:29 +0100

    add(CSSing pour le register)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,6 +8,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\EqualTo;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -18,43 +21,76 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName',TextType::class,[
+            ->add('firstName', TextType::class, [
+                'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Votre nom ne peut pas etre vide',
+                    new NotBlank(['message' => 'Votre prénom ne peut pas être vide']),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'Votre prénom doit contenir au moins {{ limit }} caractères',
+                        'maxMessage' => 'Votre prénom ne peut pas dépasser {{ limit }} caractères',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-ZÀ-ÿ\s\-]+$/u',
+                        'message' => 'Votre prénom ne peut contenir que des lettres',
                     ]),
                 ],
             ])
-            ->add('name',TextType::class,[
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Votre nom ne peut pas etre vide',
+                    new NotBlank(['message' => 'Votre nom ne peut pas être vide']),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'Votre nom doit contenir au moins {{ limit }} caractères',
+                        'maxMessage' => 'Votre nom ne peut pas dépasser {{ limit }} caractères',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-ZÀ-ÿ\s\-]+$/u',
+                        'message' => 'Votre nom ne peut contenir que des lettres',
                     ]),
                 ],
             ])
-            ->add('email')
+            ->add('email', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank(['message' => 'L\'adresse email est obligatoire']),
+                    new Email(['message' => 'Veuillez entrer une adresse email valide']),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'attr' => ['class' => 'form-check-input'],
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new IsTrue(['message' => 'Vous devez accepter nos conditions.']),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['class' => 'form-control', 'autocomplete' => 'new-password'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
+                    new NotBlank(['message' => 'Veuillez entrer un mot de passe']),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         'max' => 4096,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/',
+                        'message' => 'Votre mot de passe doit contenir au moins une lettre majuscule, un chiffre et un caractère spécial',
+                    ]),
+                ],
+            ])
+            ->add('confirmPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['class' => 'form-control', 'autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez confirmer votre mot de passe']),
+                    new EqualTo([
+                        'propertyPath' => 'plainPassword',
+                        'message' => 'Les mots de passe doivent être identiques',
                     ]),
                 ],
             ])
